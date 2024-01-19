2c852ac6926bdd694128ac3965a69aa4fdbe2515
tyler-0p1e <tyler@gongple.com>
2024-01-20 04:38:34 +0900

    add dashboard/totalCount

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -9,14 +9,14 @@ export default async function LatestInvoices({
   latestInvoices: LatestInvoice[];
 }) {
   return (
-    <div className="flex w-full flex-col md:col-span-4">
+    <div className="flex flex-col w-full md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
-      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+      <div className="flex flex-col justify-between p-4 grow rounded-xl bg-gray-50">
         {/* NOTE: comment in this code when you get to this point in the course */}
 
-        {/* <div className="bg-white px-6">
+        <div className="px-6 bg-white">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -37,7 +37,7 @@ export default async function LatestInvoices({
                     height={32}
                   />
                   <div className="min-w-0">
-                    <p className="truncate text-sm font-semibold md:text-base">
+                    <p className="text-sm font-semibold truncate md:text-base">
                       {invoice.name}
                     </p>
                     <p className="hidden text-sm text-gray-500 sm:block">
@@ -53,9 +53,9 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
-        <div className="flex items-center pb-2 pt-6">
-          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
+        </div>
+        <div className="flex items-center pt-6 pb-2">
+          <ArrowPathIcon className="w-5 h-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
         </div>
       </div>
